74e527ffcb1a8ea47849f3b760d837ef8116deaf
oleksandr <oleksandrhelevan@gmail.com>
2025-11-12 20:54:36 +0200

    Test

--- src/main/java/org/example/ecommerceproject/controller/LoginController.java
+++ src/main/java/org/example/ecommerceproject/controller/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequiredArgsConstructor
 public class LoginController {
+
     private final UserService userService;
 
     @PostMapping("/login")
